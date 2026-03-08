783eed803966d2083d1f0298a44235c36390c434
daniel alejandro neira <danielgrc85@gmail.com>
2026-03-08 15:52:29 -0500

    crear estructura de carpetas pages, components y data

---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    name: "Webcam Logitech C920",
+    price: 350000,
+    category: "Periféricos",
+    stock: 10,
+    image: "https://picsum.photos/id/20/400/300",
+    description: "Cámara web Full HD 1080p con enfoque automático y corrección de luz."
+  },
+  {
+    id: 2,
+    name: "Teclado Mecánico Keychron K2",
+    price: 480000,
+    category: "Periféricos",
+    stock: 5,
+    image: "https://picsum.photos/id/160/400/300",
+    description: "Teclado inalámbrico con switches mecánicos y retroiluminación."
+  },
+  {
+    id: 3,
+    name: "Monitor Gamer 27 pulgadas",
+    price: 1200000,
+    category: "Monitores",
+    stock: 8,
+    image: "https://picsum.photos/id/201/400/300",
+    description: "Monitor con tasa de refresco de 144Hz y resolución QHD."
+  }
+];
