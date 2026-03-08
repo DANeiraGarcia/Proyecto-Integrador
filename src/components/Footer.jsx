65584958dcf9e687f7ccb40a5e64bc5e200385ce
daniel alejandro neira <danielgrc85@gmail.com>
2026-03-08 16:58:34 -0500

    ajuste texto final en el Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2026 Ventas Prod Tecnológicos - Daniel AME & Dev</p>
+      <p>&copy; 2026 Ventas Productos Tecnológicos </p>
     </footer>
   );
 };
