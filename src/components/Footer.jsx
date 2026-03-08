963d06875fd691daa77c081507c841c1026a5684
daniel alejandro neira <danielgrc85@gmail.com>
2026-03-08 16:40:40 -0500

    feat: agregar footer básico

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2026 Ventas Prod Tecnológicos - Daniel AME & Dev</p>
+    </footer>
+  );
+};
+
+export default Footer;
